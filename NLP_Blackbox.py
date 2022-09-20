f5153eedbad8da943fbe6399567cc7d84955f0da
devkotas1 <devkotas@vcu.edu>
2022-09-19 23:53:17 -0400

    NLP Updated

--- NLP_Blackbox.py
+++ NLP_Blackbox.py
@@ -1,18 +1,17 @@
 import random
 import string
-import numpy as np
 
-import datasc
 
 
 def bx():
     words = ("pass", "fail", "review")
     num = "Random " + random.choice(words)
 
-    reason = ''.join(random.choice(string.ascii_letters) for i in range(10))
+    reason = ''.join(random.choice(string.ascii_letters) for i in range(20))
     ReasonOut = "Random Reason: " + reason
     return [num, ReasonOut]
 
 
 if __name__ == '__main__':
     bx()
+

31392ff78ff0e07167528d677895d6823de91bff
tirth2000p <80301319+tirth2000p@users.noreply.github.com>
2022-09-22 21:38:40 -0400

    secure_filenameRemoved

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import datasc
+from openpyxl import load_workbook
 from flask import Response
 import io
 buffer = io.BytesIO()
@@ -21,13 +22,19 @@ def uploader_file():
         f.save(f.filename)
         print(f.filename)
         df = datasc.data_ext(f.filename)
-        df.to_excel(buffer, index=False)
-        headers = {
-            'Content-Disposition': 'attachment; filename=output.xlsx',
-            'Content-type': 'application/vnd.ms-excel'
-        }
-        return Response(buffer.getvalue(), mimetype='application/vnd.ms-excel', headers=headers)
+        df.to_excel(buffer,index=False)
+        df.to_excel(r'out.xlsx', index=False)
 
+        book = load_workbook("out.xlsx")
+        sheet = book.active
+
+        # headers = {
+        #     'Content-Disposition': 'attachment; filename=output.xlsx',
+        #     'Content-type': 'application/vnd.ms-excel'
+        # }
+        #
+        # return Response(buffer.getvalue(), mimetype='application/vnd.ms-excel', headers=headers)
+        return render_template('s3_excel_table.html', sheet=sheet)
 
 if __name__ == '__main__':
     app.run(debug=True)
